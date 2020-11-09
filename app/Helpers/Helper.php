6b60d18dfe2e6138243e1efa3972dfba63f1d987
Nulls <wathunyu.y@gmail.com>
2020-11-09 17:10:12 +0700

    recode with helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Redis;
 
 if (!function_exists('findPlace')) {
@@ -16,6 +17,7 @@ if (!function_exists('findPlace')) {
             $API_KEY = env('GOOGLE_API_KEY', false);
             $request = env('GOOGLE_API_PLACE_HOST', false);
             $photo_request = env('GOOGLE_API_PHOTO_HOST', false);
+            $default_photo = "https://dailycliparts.com/wp-content/uploads/2019/02/Restaurant-Icon-Download-1024x842.png";
 
             $params["key"]  = $API_KEY;
             if ($next) $params["pagetoken"] = $next;
@@ -31,14 +33,13 @@ if (!function_exists('findPlace')) {
             if ($data["status"] == "OK" || $data["status"] == "ZERO_RESULTS") {
                 $result = array();
                 foreach ($data["results"] as $key => $val) { //Transform data
-                    $result["place_id"] = $val["place_id"];
-                    $result["formatted_address"] = isset($val["formatted_address"]) ? $val["formatted_address"] : '';
-                    $result["geometry"] = isset($val["geometry"]) ? $val["geometry"] : null;
-                    $result["icon"] = isset($val["icon"]) ? $val["icon"] : null;
-                    $result["name"] = isset($val["name"]) ? $val["name"] : '';
-                    $result["photos"] = isset($val["photos"]) && false ?  $photo_request . $val["photos"][0]["photo_reference"] . "&key=" . $API_KEY : "https://dailycliparts.com/wp-content/uploads/2019/02/Restaurant-Icon-Download-1024x842.png";
-                    $result["rating"] = isset($val["rating"]) ? $val["rating"] : 0;
-                    $result["user_ratings_total"] = isset($val["user_ratings_total"]) ? $val["user_ratings_total"] : 0;
+                    $result["place_id"] = Arr::get($val, "place_id", "");
+                    $result["formatted_address"] = Arr::get($val, "formatted_address", "");
+                    $result["geometry"] = Arr::get($val, "geometry", null);
+                    $result["name"] = Arr::get($val, "name", "");
+                    $result["photos"] = Arr::has($val, "photos.0.photo_reference") ? $photo_request.Arr::get($val, "photos.0.photo_reference")."&key=".$API_KEY : $default_photo;
+                    $result["rating"] = Arr::get($val, "rating", 0);
+                    $result["user_ratings_total"] = Arr::get($val, "user_ratings_total", 0);
                     array_push($results["results"], $result);
                 }
                 if (isset($data["next_page_token"])) $results["next"] = $data["next_page_token"]; //Set next page token
